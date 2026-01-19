fcdf88f856e55285e0f0943121b82e9dc7eed7eb
RaviRaj789 <raviraj.ya988@gmail.com>
2026-01-19 02:10:17 -0500

    created an basic http and ws servers

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,5 +1,30 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "./config"
+import { middleware } from "./middleware"
 
 const app = express()
+
+app.post("signup", (req,res)=>{
+
+
+})
+
+
+app.post("/signin", (req, res)=>{
+
+
+    const userId = 1 ;
+    const token = jwt.sign({userId},JWT_SECRET );
+
+    res.json({
+        token
+    })
+})
+
+app.post("/room",middleware, (req, res)=>{
+
+})
+
 
 app.listen(3001)
